b20327407a971abc808692ae30ea2f51a218e178
kpicaza <kpicaza.sound@gmail.com>
2016-01-31 19:59:06 +0100

    + Fix Apache reload requirements php5-intl

--- modules/apache/manifests/reload.pp
+++ modules/apache/manifests/reload.pp
@@ -4,6 +4,6 @@ class apache::reload {
  
   exec { 'apache reload':
     command => 'sudo service apache2 reload',	
-    require => Package['curl', 'php5-cli', 'phpmyadmin', 'php-intl']
+    require => Package['curl', 'php5-cli', 'phpmyadmin', 'php5-intl']
   }
 }
